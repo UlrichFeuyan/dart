2a2b52ec6cfe84da940b55aea33900d4733f156f
Ulrich FEUYAN <ulrich.feuyan@gmail.com>
2025-04-16 15:35:36 +0100

    Initial commit

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main(){
+    int age = 24;
+    double height = 1.7;
+    String name = "Ulrich";
+
+    List<String> Meubles = ["chaise", "canapé", "tabouret"];
+    Map<String, double> poids = {
+      "chaise": 15.5,
+      "canapé": 80.3,
+      "tabouret": 5.8,
+    };
+
+    print("Nom : $name");
+    print("Âge : $age");
+    print("Taille : $height");
+    print("Meublier : $Meubles");
+    print("Poids du meublier : $poids");
+}
